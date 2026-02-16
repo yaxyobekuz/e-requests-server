b0ed2c6b6df663cbfbf9b667d5ed0696d50c670b
yaxyobekuz <pubgn9642@gmail.com>
2026-02-16 12:11:01 +0500

    fix: update owner credentials and improve CORS configuration

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const routes = require("./src/routes");
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "1mb" }));
 app.use(mongoSanitize());
 app.use(xss());
