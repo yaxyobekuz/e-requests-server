54eff8031e86bed5c79c3e4d58eb1c7463b297b2
yaxyobekuz <pubgn9642@gmail.com>
2026-03-09 15:38:46 +0500

    feat: create adminRole model with schema for role management

---
+++ src/models/admin-role.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const adminRoleSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("AdminRole", adminRoleSchema);
